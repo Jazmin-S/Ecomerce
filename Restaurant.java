29a02a861520bfa0b83a6b653b73a4c296e8ba9c
Jazmin <zs23014159@estudiantes.uv.mx>
2025-03-02 00:20:11 -0600

    Add files via upload
    Codigo principal de Restaurante

---
+++ Restaurant.java
@@ -0,0 +1,18 @@
+package restaurant;
+
+import vistas.MeserosView;
+
+/**
+ *
+ * @author gaelr
+ */
+public class Restaurant {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new MeserosView().setVisible(true);
+    }
+    
+}
